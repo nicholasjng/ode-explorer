7686507f172da836a708cf330100fcb404c3753e
Nicholas Junge <nicholas.junge@web.de>
2020-10-01 16:44:29 +0200

    More dict format stuff, almost done

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,42 +1,68 @@
 import numpy as np
 import pandas as pd
 import os
-import datetime
 from ode_explorer.model import ODEModel
-from typing import List, Dict, Text, Any
+from ode_explorer.constants import ZIPPED, VARIABLES
+from typing import List, Dict, Text, Any, Union
 
 
 def make_log_dir():
     pass
 
 
-def isscalar(y):
-    return hasattr(y, "__len__")
+def is_scalar(y):
+    return not hasattr(y, "__len__")
 
 
 def convert_to_zipped(state_dict: Dict[Text, Any], model: ODEModel):
     t = state_dict[model.indep_name]
-    y = state_dict[model.variable_name]
+    y = state_dict[model.variable_names[0]]
+
     # small check for a default value in the scalar case (n = 1)
-    if not hasattr(y, "__len__"):
-        return {model.indep_name: t, **dict(zip(model.dim_names, [y]))}
+    if is_scalar(y):
+        return {model.indep_name: t, model.dim_names[0]: y}
     return {model.indep_name: t, **dict(zip(model.dim_names, y))}
 
 
 def convert_from_zipped(state_dict: Dict[Text, Any], model: ODEModel):
     t = state_dict[model.indep_name]
     y = np.array([state_dict[key] for key in model.dim_names])
+
     if len(model.dim_names) == 1:
-        return {model.indep_name: t, model.variable_name: y[0]}
-    return {model.indep_name: t, model.variable_name: y}
+        return {model.indep_name: t, model.variable_names[0]: y[0]}
+    return {model.indep_name: t, model.variable_names[0]: y}
 
 
-def convert_state_dict(state_dict: Dict[Text, Any], model: ODEModel):
+def convert_state_dict(state_dict: Dict[Text, Any], model: ODEModel,
+                       input_format: Text = None,
+                       output_format: Text = ZIPPED):
     # infer dict mode by presence of variable name
-    if model.variable_name in state_dict.keys():
+    if not input_format:
+        input_format = infer_dict_format(state_dict=state_dict, model=model)
+
+    if input_format == output_format:
+        return state_dict
+
+    if output_format == ZIPPED:
         return convert_to_zipped(state_dict=state_dict, model=model)
     else:
         return convert_from_zipped(state_dict=state_dict, model=model)
+
+
+def infer_dict_format(state_dict: Dict[Text, Union[float, np.ndarray]],
+                      model: ODEModel):
+
+    if all(var in state_dict for var in model.variable_names):
+        # this is the case of a scalar ODE, where ZIPPED and VARIABLES are
+        # the same
+        if not any(isinstance(v, np.ndarray) for v in state_dict.values()):
+            return ZIPPED
+        return VARIABLES
+    elif all(dim in state_dict for dim in model.dim_names):
+        return ZIPPED
+    else:
+        raise ValueError("Error: Unrecognizable state dict format. Something "
+                         "went wrong.")
 
 
 def write_to_file(result_data: List[Dict[Text, float]],

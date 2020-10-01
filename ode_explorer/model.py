472f465447dfc4a2977ba1f0f7b8cd6d730038ee
Nicholas Junge <nicholas.junge@web.de>
2020-10-01 16:43:55 +0200

    Fixed variable names logic

--- ode_explorer/model.py
+++ ode_explorer/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Callable, Dict, Any, Text, List, Union
 from utils.import_utils import import_func_from_module
+from utils.data_utils import is_scalar
 
 
 class ODEModel:
@@ -15,7 +16,7 @@ class ODEModel:
                                   Union[float, np.ndarray]] = None,
                  fn_args: Dict[Text, Any] = None,
                  indep_name: Text = None,
-                 variable_name: Text = None,
+                 variable_names: List[Text] = None,
                  dim_names: List[Text] = None):
 
         # ODE function, right hand side of y' = f(t,y)
@@ -40,7 +41,7 @@ class ODEModel:
         self.fn_args = fn_args
 
         self.indep_name = indep_name or "t"
-        self.variable_name = variable_name or "y"
+        self.variable_names = variable_names or ["y"]
         self.dim_names = dim_names
 
     def update_args(self, **kwargs):
@@ -50,11 +51,14 @@ class ODEModel:
             self.fn_args = kwargs
 
     def make_initial_state(self, initial_time: float, initial_vec: Any):
-        return {self.indep_name: initial_time, self.variable_name: initial_vec}
+        return {self.indep_name: float(initial_time),
+                **{var: val for var, val in
+                   zip(self.variable_names, initial_vec)}}
 
     def initialize_dim_names(self, initial_state: Dict[Text, Any]):
-        if not any(hasattr(v, "__len__") for v in initial_state.values()):
+        if all(is_scalar(v) for v in initial_state.values()):
             num_dims = len(list(initial_state.keys())) - 1
+
         else:
             num_dims = -1  # account for time
             for v in initial_state.values():
@@ -68,15 +72,18 @@ class ODEModel:
         if self.dim_names and len(self.dim_names) != num_dims:
             raise ValueError("Error: Dimension mismatch. List of dimension "
                              "names suggests a system of size {0}, but "
-                             "inferred system size {1} from initial state."
-                             "".format(len(self.dim_names), num_dims))
+                             "inferred a system size of {1} from initial "
+                             "state.".format(len(self.dim_names), num_dims))
 
         if not self.dim_names:
             if num_dims == 1:
-                self.dim_names = list(self.variable_name)
+                self.dim_names = self.variable_names
             else:
-                self.dim_names = ["{0}_{1}".format(self.variable_name, i)
-                                  for i in range(1, num_dims + 1)]
+                dims = []
+                for var in self.variable_names:
+                    dims += ["{0}_{1}".format(var, i)
+                             for i in range(1, num_dims + 1)]
+                self.dim_names = dims
 
     def __call__(self, t: float, y: np.ndarray, **kwargs) -> np.ndarray:
         """

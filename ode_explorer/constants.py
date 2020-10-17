e17a7ed3ee8f84a8b1ce05c25b8c1cc315f5e860
Nicholas Junge <nicholas.junge@web.de>
2020-10-17 19:02:41 +0200

    Dynamic integration constants

--- ode_explorer/constants.py
+++ ode_explorer/constants.py
@@ -1,3 +1,8 @@
 # dict formats, used for writing and displaying ODE integration data
 ZIPPED = "zipped"
 VARIABLES = "variables"
+
+
+# dynamic (step size integration) variables)
+DYNAMIC_MAX_STEPS = 10000
+DYNAMIC_INITIAL_H = 0.01

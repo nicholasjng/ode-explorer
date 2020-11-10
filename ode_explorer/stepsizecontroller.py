0efe0ee65ed788ca1350759ae68e1bac6da2351d
Nicholas Junge <nicholas.junge@web.de>
2020-11-10 22:51:20 +0100

    Complete rework and implementation after Hairer/Wanner

--- ode_explorer/stepsizecontroller.py
+++ ode_explorer/stepsizecontroller.py
@@ -15,11 +15,19 @@ class StepsizeController:
 
 
 class DOPRI45Controller(StepsizeController):
-    def __init__(self, fac_min: float, fac_max: float, tol: float,
+    def __init__(self,
+                 atol: float,
+                 rtol: float,
+                 fac_min: float = 0.2,
+                 fac_max: float = 5.0,
                  safety_factor: float = 0.9):
+
+        self.atol = atol
+        self.rtol = rtol
+        # maximal step size reduction factor
         self.fac_min = fac_min
+        # maximal step size increase factor
         self.fac_max = fac_max
-        self.tol = tol
         self.safety_factor = safety_factor
         self.order = 5
 
@@ -35,17 +43,19 @@ class DOPRI45Controller(StepsizeController):
 
         var_name = model.variable_names[0]
 
+        y_prev = state[var_name]
         y_4, y_5 = order4[var_name], order5[var_name]
 
-        norm_diff45 = np.linalg.norm(y_4 - y_5)
-        accept = norm_diff45 < self.tol
+        err_tol = self.atol + self.rtol * np.maximum(np.abs(y_prev),
+                                                     np.abs(y_5))
 
-        error_est = (self.tol / h * norm_diff45) ** (1 / self.order)
+        err_ratio = np.linalg.norm(y_4 / err_tol)
+
+        accept = err_ratio < 1.
+
+        error_est = (1 / err_ratio) ** (1 / self.order)
 
         h_new = h * min(self.fac_max,
                         max(self.fac_min, self.safety_factor * error_est))
-
-        if not accept:
-            h_new = 0.5 * h
 
         return accept, h_new

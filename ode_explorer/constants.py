381d49be92cea5afb07c1e1ac241a64286c85301
Nicholas Junge <nicholas.junge@web.de>
2020-11-19 21:47:01 +0100

    Constants grouped by use case

--- ode_explorer/constants.py
+++ ode_explorer/constants.py
@@ -1,8 +1,24 @@
 # dict formats, used for writing and displaying ODE integration data
-ZIPPED = "zipped"
-VARIABLES = "variables"
+class DataFormatKeys:
+    ZIPPED = "zipped"
+    VARIABLES = "variables"
 
 
 # dynamic (step size integration) variables)
-DYNAMIC_MAX_STEPS = 10000
-DYNAMIC_INITIAL_H = 0.01
+class DynamicVariables:
+    DYNAMIC_MAX_STEPS = 10000
+    DYNAMIC_INITIAL_H = 0.01
+
+
+class RunKeys:
+    RESULT_DATA = "result_data"
+    METRICS = "metrics"
+    RUN_METADATA = "run_metadata"
+
+
+class RunMetadataKeys:
+    METRIC_NAMES = "metric_names"
+    CALLBACK_NAMES = "callback_names"
+    DIM_NAMES = "dim_names"
+    VARIABLE_NAMES = "variable_names"
+    TIMESTAMP = "timestamp"

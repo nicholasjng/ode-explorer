9b127cdb1d331977269592d9fe6c7947b2e3a656
Nicholas Junge <nicholas.junge@web.de>
2020-11-19 21:45:44 +0100

    Add init and name

--- ode_explorer/metrics.py
+++ ode_explorer/metrics.py
@@ -5,6 +5,10 @@ from typing import Dict, Text, Any, Callable, Union
 
 
 class Metric:
+
+    def __init__(self, name: Text = None):
+        self.__name__ = name or self.__class__.__name__
+
     def __call__(self,
                  i: int,
                  state: Dict[Text, Union[np.ndarray, float]],
@@ -20,12 +24,13 @@ class DistanceToSolution(Metric):
     solution. This is useful to test whether an integrator performs as
     expected.
     """
-    def __init__(self, solution: Callable, norm: int = None,
+    def __init__(self, solution: Callable, norm: Union[Text, int] = None,
                  name: Text = None):
+
+        super(DistanceToSolution, self).__init__(name=name)
 
         self.solution = solution
         self.norm = norm or 2
-        self.name = name or "solution_distance"
 
     def __call__(self,
                  i: int,
@@ -33,8 +38,6 @@ class DistanceToSolution(Metric):
                  updated_state: Dict[Text, Union[np.ndarray, float]],
                  model: ODEModel,
                  locals: Dict[Text, Any]) -> Any:
-
-        updated_state = locals["updated_state_dict"]
 
         t = updated_state[model.indep_name]
         y = np.array([updated_state[key] for key in model.dim_names])

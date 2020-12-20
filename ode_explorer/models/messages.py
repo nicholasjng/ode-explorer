0d478f483b542b99b1cfb4e5337a0eba50ed7c6c
Nicholas Junge <nicholas.junge@web.de>
2020-12-20 21:42:13 +0100

    Error messages in separate file

---
+++ ode_explorer/models/messages.py
@@ -0,0 +1,10 @@
+
+MISSING_INFO = "Missing model information. Supply a right hand side f(t,y) " \
+               "either by specifying a source path or a callable function."
+
+BAD_MODEL_DEF = "Defining a model function by a source path and by a callable function " \
+                "object are mutually exclusive options. Please choose only one of these options."
+
+DIMENSION_MISMATCH = "Error: Dimension mismatch. List of dimension names " \
+                     "suggests a system of size {0}, but inferred a system size of {1} " \
+                     "from initial state."

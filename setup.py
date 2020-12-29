720e3627ed88e5c53a42d2cec07138474028051d
Nicholas Junge <nicholas.junge@web.de>
2020-12-30 00:16:58 +0100

    Additional identifiers

--- setup.py
+++ setup.py
@@ -16,9 +16,20 @@ setuptools.setup(
     url="https://github.com/njunge94/ode-explorer",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     python_requires='>=3.6',
 )

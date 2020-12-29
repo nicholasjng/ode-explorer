199150650399f2abd82af5307793539a30fe725c
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 12:51:00 +0100

    IDE screwed up

--- ode_explorer/models/model.py
+++ ode_explorer/models/model.py
@@ -3,8 +3,8 @@ from typing import Dict, Any, Text, List, Callable
 from ode_explorer.constants import ModelMetadataKeys
 from ode_explorer.models import BaseModel
 from ode_explorer.models import messages
-from ode_explorer.types import ModelState, StateVariable
-from ode_explorer.utils.helpers import is_scalar, infer_variable_names
+from ode_explorer.types import StateVariable
+from ode_explorer.utils.helpers import infer_variable_names
 from ode_explorer.utils.import_utils import import_func_from_module
 
 AdditionalArgs = Any
@@ -47,8 +47,6 @@ class ODEModel(BaseModel):
 
     def make_state(self, time: StateVariable, vec: StateVariable):
         return time, vec
-
-
 
     def get_metadata(self):
 

1aafcc70f7a27a81ecdbdd8990f0cbb90af0c5e8
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 12:37:50 +0100

    Removed legacy classes

--- ode_explorer/stepfunctions/__init__.py
+++ ode_explorer/stepfunctions/__init__.py
@@ -1,9 +1,7 @@
 from ode_explorer.stepfunctions.stepfunctions import (
     EulerMethod,
-    EulerCython,
     HeunMethod,
     RungeKutta4,
-    DOPRI5,
     DOPRI45,
     ImplicitEulerMethod,
     AdamsBashforth2,

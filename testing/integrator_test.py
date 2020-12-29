ff3493717fb99b7803643f3ebea926d5287efdf4
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 14:16:14 +0100

    Add scalar vs vector ODE testing

--- testing/integrator_test.py
+++ testing/integrator_test.py
@@ -6,8 +6,8 @@ from ode_explorer.integrators import Integrator
 from ode_explorer.stepsize_control import DOPRI45Controller
 from ode_explorer.metrics import DistanceToSolution
 
-# y_0 = 1.0
-y_0 = np.ones(10)
+y_0_scalar = 1.0
+y_0_vec = np.ones(10)
 lamb = 0.5
 
 
@@ -15,8 +15,12 @@ def ode_func(t: float, y: Union[float, np.ndarray], lamb: float = 0.5):
     return - lamb * y
 
 
-def sol(t):
-    return y_0 * np.exp(-lamb * t)
+def sol_scalar(t):
+    return y_0_scalar * np.exp(-lamb * t)
+
+
+def sol_vec(t):
+    return y_0_vec * np.exp(-lamb * t)
 
 
 def main():
@@ -33,35 +37,38 @@ def main():
 
     integrator = Integrator()
 
-    initial_state = (t_0, y_0)
+    for initial in [(y_0_scalar, sol_scalar), (y_0_vec, sol_vec)]:
+        initial_y, sol = initial
+        initial_state = (t_0, initial_y)
 
-    for step_func in step_list:
-        integrator.integrate_const(model=model,
-                                   step_func=step_func,
-                                   initial_state=initial_state,
-                                   h=0.001,
-                                   max_steps=10000,
-                                   verbosity=1,
-                                   progress_bar=True,
-                                   metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
+        for step_func in step_list:
+            step_func.reset()
+            integrator.integrate_const(model=model,
+                                       step_func=step_func,
+                                       initial_state=initial_state,
+                                       h=0.001,
+                                       max_steps=10000,
+                                       verbosity=1,
+                                       progress_bar=True,
+                                       metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
+
+            metrics = integrator.return_metrics(run_id="latest")
+
+            print(metrics.describe())
+
+        integrator.integrate_dynamically(model=model,
+                                         step_func=DOPRI45(),
+                                         sc=DOPRI45Controller(atol=1e-9),
+                                         initial_state=initial_state,
+                                         initial_h=0.001,
+                                         verbosity=1,
+                                         end=10.0,
+                                         progress_bar=True,
+                                         metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
 
         metrics = integrator.return_metrics(run_id="latest")
 
         print(metrics.describe())
-
-    integrator.integrate_dynamically(model=model,
-                                     step_func=DOPRI45(),
-                                     sc=DOPRI45Controller(atol=1e-9),
-                                     initial_state=initial_state,
-                                     initial_h=0.01,
-                                     verbosity=1,
-                                     end=10.0,
-                                     progress_bar=True,
-                                     metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
-
-    metrics = integrator.return_metrics(run_id="latest")
-
-    print(metrics.describe())
 
 
 if __name__ == "__main__":
--- testing/rk_template_test.py
+++ testing/rk_template_test.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from ode_explorer.stepfunctions import *
-from ode_explorer.stepfunctions import ExplicitRungeKuttaMethod
-from ode_explorer.models import ODEModel
 from ode_explorer.integrators import Integrator
 from ode_explorer.metrics import DistanceToSolution
+from ode_explorer.models import ODEModel
+from ode_explorer.stepfunctions import *
+from ode_explorer.stepfunctions import ExplicitRungeKuttaMethod
 
 # y_0 = 1.0
 y_0 = np.ones(10)
@@ -26,42 +26,45 @@ def main():
 
     integrator = Integrator()
 
-    alphas = np.array([0.0, 0.5, 0.5, 1.0])
-    betas = np.array([[0.0, 0.0, 0.0, 0.0],
-                      [0.5, 0.0, 0.0, 0.0],
-                      [0.0, 0.5, 0.0, 0.0],
-                      [0.0, 0.0, 1.0, 0.0]])
-    gammas = np.array([1.0, 2.0, 2.0, 1.0]) / 6
+    step_funcs = []
 
-    step2 = ExplicitRungeKuttaMethod(alphas=alphas, betas=betas, gammas=gammas)
+    rk4_alphas = np.array([0.0, 0.5, 0.5, 1.0])
+    rk4_betas = np.array([[0.0, 0.0, 0.0, 0.0],
+                          [0.5, 0.0, 0.0, 0.0],
+                          [0.0, 0.5, 0.0, 0.0],
+                          [0.0, 0.0, 1.0, 0.0]])
+    rk4_gammas = np.array([1.0, 2.0, 2.0, 1.0]) / 6
+
+    templated_rk4 = ExplicitRungeKuttaMethod(alphas=rk4_alphas, betas=rk4_betas, gammas=rk4_gammas)
+
+    step_funcs.append(templated_rk4)
+
+    c = np.sqrt(3) / 6
+
+    i_alphas = np.array([0.5 - c, 0.5 + c])
+    i_betas = np.array([[0.25, 0.25 - c],
+                        [0.25 + c, 0.25]])
+    i_gammas = np.array([0.5, 0.5])
+
+    gauss_lobatto2 = ImplicitRungeKuttaMethod(alphas=i_alphas, betas=i_betas, gammas=i_gammas)
+
+    step_funcs.append(gauss_lobatto2)
 
     initial_state = (t_0, y_0)
 
-    integrator.integrate_const(model=model,
-                               step_func=RungeKutta4(),
-                               initial_state=initial_state,
-                               h=0.001,
-                               max_steps=10000,
-                               verbosity=1,
-                               progress_bar=True,
-                               metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
+    for step_func in step_funcs:
+        integrator.integrate_const(model=model,
+                                   step_func=step_func,
+                                   initial_state=initial_state,
+                                   h=0.001,
+                                   max_steps=10000,
+                                   verbosity=1,
+                                   progress_bar=True,
+                                   metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
 
-    metrics = integrator.return_metrics(run_id="latest")
+        metrics = integrator.return_metrics(run_id="latest")
 
-    print(metrics.describe())
-
-    integrator.integrate_const(model=model,
-                               step_func=step2,
-                               initial_state=initial_state,
-                               h=0.001,
-                               max_steps=10000,
-                               verbosity=1,
-                               progress_bar=True,
-                               metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
-
-    metrics = integrator.return_metrics(run_id="latest")
-
-    print(metrics.describe())
+        print(metrics.describe())
 
 
 if __name__ == "__main__":

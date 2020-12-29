7d0db60175b9e8283ad784486bff7364a21cb1a9
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 19:55:26 +0100

    Metrics docstrings

--- ode_explorer/metrics/metric.py
+++ ode_explorer/metrics/metric.py
@@ -7,8 +7,19 @@ from ode_explorer.types import ModelState
 
 
 class Metric:
+    """
+    Base metric interface. Subclass this to define your own metrics,
+    to be computed during ODE integration.
+    """
 
     def __init__(self, name: Text = None):
+        """
+        Base metric constructor.
+
+        Args:
+            name: Optional name identifier. This is the name that will be displayed
+             in metrics data frames obtained from integration runs.
+        """
         self.__name__ = name or self.__class__.__name__
 
     def __call__(self,
@@ -31,6 +42,14 @@ class DistanceToSolution(Metric):
                  solution: Callable,
                  norm: Union[Text, int] = None,
                  name: Text = None):
+        """
+        Solution distance metric constructor.
+
+        Args:
+            solution: Callable, of signature t -> y(t) giving the ODE solution at time t.
+            norm: Norm identifier for use in np.linalg.norm.
+            name: Optional name identifier.
+        """
         super(DistanceToSolution, self).__init__(name=name)
 
         self.solution = solution
@@ -42,6 +61,20 @@ class DistanceToSolution(Metric):
                  updated_state: ModelState,
                  model: ODEModel,
                  local_vars: Dict[Text, Any]) -> Any:
+        """
+        Solution distance call operator overload.
+
+        Args:
+            i: Current iteration number.
+            state: Previous ODE model state.
+            updated_state: New calculated ODE model state.
+            model: ODE model that is being integrated.
+            local_vars: Handle for locals() dict object.
+
+        Returns:
+            A scalar, the norm difference between the calculated state and the theoretical solution.
+
+        """
         t, y = updated_state
 
         y_pred = self.solution(t)

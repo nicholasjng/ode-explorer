668cd8123e6afff8d0a28301576b0ed5ac6ec286
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 20:26:44 +0100

    Deepcopy to avoid messing up the saved run

--- ode_explorer/utils/run_utils.py
+++ ode_explorer/utils/run_utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import copy
 from typing import Dict, Text
 
 from ode_explorer import constants
@@ -24,21 +25,25 @@ def get_run_metadata(run):
     return metadata
 
 
-def write_run_to_disk(run: Dict, out_dir: Text):
+def write_run_to_disk(run: Dict, out_dir: Text, **kwargs):
     """
     Save a run to disk, including result data, metrics and additional info.
 
     Args:
         run: Run object saved in an Integrator instance.
         out_dir: Designated output directory.
+        **kwargs: Additional keyword arguments passed to pandas.DataFrame.to_csv.
     """
-    result_data = run[RunKeys.RESULT_DATA]
 
-    metric_data = run[RunKeys.METRICS]
+    run_copy = copy.deepcopy(run)
+
+    result_data = run_copy.pop(RunKeys.RESULT_DATA)
+
+    metric_data = run_copy.pop(RunKeys.METRICS)
 
     run_filename = "run_info.json"
 
-    model_metadata = run[RunKeys.MODEL_METADATA]
+    model_metadata = run_copy[RunKeys.MODEL_METADATA]
 
     variable_names = model_metadata[ModelMetadataKeys.VARIABLE_NAMES]
 
@@ -54,13 +59,15 @@ def write_run_to_disk(run: Dict, out_dir: Text):
     # write result vectors to csv file
     write_result_to_csv(result=result_data,
                         out_dir=out_dir,
-                        outfile_name=RunKeys.RESULT_DATA)
+                        outfile_name=RunKeys.RESULT_DATA,
+                        **kwargs)
 
     # write metrics to csv file
     write_result_to_csv(result=metric_data,
                         out_dir=out_dir,
-                        outfile_name=RunKeys.METRICS)
+                        outfile_name=RunKeys.METRICS,
+                        **kwargs)
 
     outfile = os.path.join(out_dir, run_filename)
     with open(outfile, "w") as f:
-        json.dump(run, f)
+        json.dump(run_copy, f)

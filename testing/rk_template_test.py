6a3348b061b7e07942b57cbbb027a94a27bce642
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 14:29:06 +0100

    RK Templates tested and working

--- testing/rk_template_test.py
+++ testing/rk_template_test.py
@@ -6,8 +6,8 @@ from ode_explorer.models import ODEModel
 from ode_explorer.stepfunctions import *
 from ode_explorer.stepfunctions import ExplicitRungeKuttaMethod
 
-# y_0 = 1.0
-y_0 = np.ones(10)
+y_0_scalar = 1.0
+y_0_vec = np.ones(10)
 lamb = 0.5
 
 
@@ -15,8 +15,12 @@ def ode_func(t: float, y: Union[float, np.ndarray], lamb: float = 0.5):
     return - lamb * y
 
 
-def sol(t):
-    return y_0 * np.exp(-lamb * t)
+def sol_scalar(t):
+    return y_0_scalar * np.exp(-lamb * t)
+
+
+def sol_vec(t):
+    return y_0_vec * np.exp(-lamb * t)
 
 
 def main():
@@ -50,21 +54,24 @@ def main():
 
     step_funcs.append(gauss_lobatto2)
 
-    initial_state = (t_0, y_0)
+    for initial in [(y_0_scalar, sol_scalar), (y_0_vec, sol_vec)]:
+        initial_y, sol = initial
+        initial_state = (t_0, initial_y)
 
-    for step_func in step_funcs:
-        integrator.integrate_const(model=model,
-                                   step_func=step_func,
-                                   initial_state=initial_state,
-                                   h=0.001,
-                                   max_steps=10000,
-                                   verbosity=1,
-                                   progress_bar=True,
-                                   metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
+        for step_func in step_funcs:
+            step_func.reset()
+            integrator.integrate_const(model=model,
+                                       step_func=step_func,
+                                       initial_state=initial_state,
+                                       h=0.001,
+                                       max_steps=10000,
+                                       verbosity=1,
+                                       progress_bar=True,
+                                       metrics=[DistanceToSolution(solution=sol, name="l2_distance")])
 
-        metrics = integrator.return_metrics(run_id="latest")
+            metrics = integrator.return_metrics(run_id="latest")
 
-        print(metrics.describe())
+            print(metrics.describe())
 
 
 if __name__ == "__main__":

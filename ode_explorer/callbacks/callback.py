fa4b459d99f4438fdc8aa78bbfa6e01ce294ad72
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 16:57:11 +0100

    Callbacks docstrings

--- ode_explorer/callbacks/callback.py
+++ ode_explorer/callbacks/callback.py
@@ -10,7 +10,17 @@ logger = logging.getLogger(__name__)
 
 
 class Callback:
+    """
+    Base callback interface. Callbacks can be used to change the control flow of an
+    integration run for an ODE model.
+    """
     def __init__(self, name: Text = None):
+        """
+        Base callback constructor.
+
+        Args:
+            name: Optional string identifier.
+        """
         self.__name__ = name or self.__class__.__name__
 
     def __call__(self,
@@ -19,19 +29,43 @@ class Callback:
                  updated_state: ModelState,
                  model: ODEModel,
                  local_vars: Dict[Text, Any]) -> None:
+        """
+        Callback class call operator. Overload this with your custom logic to use in
+        ODE integration runs.
+
+        Args:
+            i: Current iteration number.
+            state: Previous ODE state.
+            updated_state: New ODE state calculated by the used step function.
+            model: ODE model that is used in the integration run.
+            local_vars: Handle for the locals() dict passed to the Callbacks.
+        """
         raise NotImplementedError
 
 
 class NaNChecker(Callback):
+    """
+    Custom callback to check for NaNs in the ODE integration data.
+    The occurrence of NaN values points to an instability in the
+    step function or in the model function that was used.
+    """
     def __init__(self,
-                 fill_nans: bool = False,
-                 replacement: float = 0.0,
                  nan_handling_mode: Text = "raise",
+                 replacement: float = 0.0,
                  name: Text = None):
+        """
+        NaN checking callback constructor. The NaN handling mode is specified upon construction.
+        Options are ``raise`` (a NaN value raises an error), ``coerce`` (NaN values are filled with a
+        replacement value), or ``ignore`` (NaN values are propagated as-is).
+
+        Args:
+            nan_handling_mode: String, how to handle occurring NaN values.
+            replacement: Replacement value for NaN values if nan_handling_mode was set to "coerce".
+            name: Optional string identifier.
+        """
 
         super(NaNChecker, self).__init__(name=name)
 
-        self.fill_nans = fill_nans
         self.replacement = replacement
         nan_handling_options = ["raise", "coerce", "ignore"]
         if nan_handling_mode not in nan_handling_options:
@@ -45,6 +79,20 @@ class NaNChecker(Callback):
                  updated_state: ModelState,
                  model: ODEModel,
                  local_vars: Dict[Text, Any]) -> None:
+        """
+        Control flow of the NaN checking callback.
+
+        Args:
+            i: Current iteration number.
+            state: Previous ODE state.
+            updated_state: New ODE state calculated by the used step function.
+            model: ODE model that is used in the integration run.
+            local_vars: Handle for the locals() dict passed to the Callbacks.
+
+        Raises:
+            ValueError: If any NaN values were found and the NaN handling mode was set to "raise".
+
+        """
 
         # only check new state as old is assumed to be sensible,
         # i.e. not having nans

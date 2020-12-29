4636a5962046a53fa049871b2dc98b73d0c5108d
Nicholas Junge <nicholas.junge@web.de>
2020-12-29 19:55:47 +0100

    Base model docstring

--- ode_explorer/models/base_model.py
+++ ode_explorer/models/base_model.py
@@ -1,7 +1,24 @@
 class BaseModel:
+    """
+    Base model class. Override this to define your own ODE model classes.
+    """
 
     def make_state(self, *args, **kwargs):
+        """
+        Constructs a state object from raw input floats and numpy arrays.
+
+        Returns:
+            A state object holding the input data.
+
+        """
         pass
 
     def __call__(self, *args, **kwargs):
+        """
+        BaseModel call operator. Overload this to use your model with builtin step functions.
+
+        Returns:
+            A state vector corresponding to the right hand side of y' = f(t,y).
+
+        """
         raise NotImplementedError
